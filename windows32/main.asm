d8f5c2d4918f641e08167da31e891730d5ee3033
Michael Dorst <code@mikefdorst.com>
2016-10-13 15:58:58 -0700

    Blank

--- windows32/main.asm
+++ windows32/main.asm
@@ -7,64 +7,16 @@ INCLUDE io.h   ; header file for input/output
 
 .DATA
 	
-	number1 DWORD 0
-	number2 DWORD 0
-	prompt1 BYTE "Enter first number x", 0
-	prompt2 BYTE "Enter second number y", 0
-	string BYTE 20 DUP (0)
-	resultLabel BYTE "3 * x + 7 * y", 0
-	result BYTE 11 DUP (0), 0
+	
 
 .CODE
 _MainProc PROC
 
-	; Get number1 from user
-	input prompt1, string, 20
-	atod string
-	mov number1, eax
-
-	; Get number2 from user
-	input prompt2, string, 20
-	atod string
-	mov number2, eax
-
-	push number2				; 2nd parameter
-	push number1				; 1st parameter
-	call func1					; func1(number1, number2)
-	add esp, 8					; remove parameters from stack
-
-	; Display result
-	dtoa result, eax
-	output resultLabel, result
+	
 
 	mov eax, 0					; exit with return code 0
 	
 	ret
 _MainProc ENDP
-
-; int func1 (int, int)
-; returns 3 * x + 7 * y
-func1 PROC
-	
-	push ebp					; save base pointer
-	mov ebp, esp				; establish stack frame
-	push ebx					; save ebx
-
-	; The saved ebp and ebx values lie between the current ebp location and the first parameter
-	; Stack values are stored as DWORDs, so 4 bytes each.
-	; That makes the first parameter (x) [ebp+8]
-	; and the second parameter (y) [ebp+12]
-
-	mov eax, [ebp+8]			; eax := x
-	imul eax, 3					; eax := x * 3
-	mov ebx, [ebp+12]			; ebx := y
-	imul ebx, 7					; ebx := y * 7
-	add eax, ebx				; eax := x * 3 + y * 7
-
-	pop ebx						; restore ebx
-	pop ebp						; restore base pointer
-
-	ret							; return
-func1 ENDP
 
 END   ; end of source code
