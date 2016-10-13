c73b26cdb6f2f413c1b7ba89f8efd417f6878105
Michael Dorst <code@mikefdorst.com>
2016-10-13 15:25:17 -0700

    Example program

--- windows32/main.asm
+++ windows32/main.asm
@@ -7,71 +7,59 @@ INCLUDE io.h   ; header file for input/output
 
 .DATA
 	
-	; We are using DWORD sizes, because the problem spec says to.
-	; We are using unsing unsigned because the problem says the two numbers are positive
-	
-	; These prompts will be displayed when asking for numbers
-	; They are null terminated because the input macro expects a C-string
-	number1Prompt BYTE "Please input a positive integer", 0
-	number2Prompt BYTE "Please input another positive integer", 0
-
-	; inputString will be used to store the user's inputs in ASCII.
-	; It is 11 characters long because the longest input will be "4294967295" (11 characters) and the input
-	; macro appends a trailing null (11)
-	; we tried this with 10 bytes, but because of the trailing null the final character of a 10 character number would get cut off,
-	; leaving only 9 characters to represent the number.
-	inputString BYTE 11 DUP ("X")
-
-	; gcdString will be used with dtoa, which expects 11 characters + Null terminator
-	gcdString BYTE 11 DUP ("X"), 0	; String to store gcd in decimal for output
-	
-	; gcdOutputLabel will be used to label the gcd when it is displayed
-	gcdOutputLabel BYTE "Greatest Common Divisor"
-
+	number1 DWORD 0
+	number2 DWORD 0
+	prompt1 BYTE "Enter first number x", 0
+	prompt2 BYTE "Enter second number y", 0
+	string BYTE 20 DUP (0)
+	resultLabel BYTE "3 * x + 7 * y", 0
+	result BYTE 11 DUP (0), 0
 
 .CODE
 _MainProc PROC
-	
-	; gcd := number1
-	; remainder := number2
-	; loopStart
-	;   dividend := gcd
-	;   gcd := remainder
-	;   remainder := dividend mod gcd
-	; until(remainder = 0)
 
-	; We are using DWORD sizes, because the problem spec says to.
-	; ebx = gcd
-	; ecx = remainder
-	; eax = dividend
+	; Get number1 from user
+	input prompt1, string, 20
+	atod string
+	mov number1, eax
 
-	input number1Prompt, inputString, 11	; get input from user (maximum 11 characters, as explained above at inputString)
-	atod inputString						; convert input from ASCII coded decimal to binary integer (stored in EAX)
-	mov ebx, eax							; gcd := user input
+	; Get number2 from user
+	input prompt2, string, 20
+	atod string
+	mov number2, eax
 
-	input number2Prompt, inputString, 11	; get input from user
-	atod inputString						; convert input from string to binary integer (stored in EAX)
-	mov ecx, eax							; remainder := user input
+	push number2				; 2nd parameter
+	push number1				; 1st parameter
+	call func1					; func1(number1, number2)
+	add esp, 8					; remove parameters from stack
 
-loopStart:
-	mov eax, ebx							; dividend := gcd
-	mov ebx, ecx							; gcd := remainder
-	
-	mov edx, 0								; EDX will be used as the higher order bits for the dividend
-											; we must set EDX to 0 before dividing because the higher order bits are not significant
-	div ebx									; divide dividend by gcd (to get dividend mod gcd)
-	mov ecx, edx							; remainder := dividend mod gcd
-	cmp ecx, 0								; remainder = 0?
-	jnz loopStart							; if (remainder != 0) goto loopStart
-loopEnd:
-	
-	dtoa gcdString, ebx						; convert gcd to string (for output)
-	output gcdOutputLabel, gcdString		; output gcd
+	; Display result
+	dtoa result, eax
+	output resultLabel, result
 
-
-	mov eax, 0								; exit with return code 0
+	mov eax, 0					; exit with return code 0
 	
 	ret
 _MainProc ENDP
+
+; int func1 (int, int)
+; returns 3 * x + 7 * y
+func1 PROC
+	
+	push ebp					; save base pointer
+	mov ebp, esp				; establish stack frame
+	push ebx					; save ebx
+
+	mov eax, [ebp+8]			; eax := x
+	imul eax, 3					; eax := x * 3
+	mov ebx, [ebp+12]			; ebx := y
+	imul ebx, 7					; ebx := y * 7
+	add eax, ebx				; eax := x * 3 + y * 7
+
+	pop ebx						; restore ebx
+	pop ebp						; restore base pointer
+
+	ret							; return
+func1 ENDP
 
 END   ; end of source code
